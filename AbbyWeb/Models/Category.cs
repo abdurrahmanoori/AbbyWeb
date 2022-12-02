8ba086393f6de2e6e0d67d80ebe743be8aa47314
Abdurrahman <Abdurrahmannoori10@gmail.com>
2022-12-02 07:58:29 +0430

    Add project files.

---
+++ AbbyWeb/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AbbyWeb.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Display Order")]
+        [Range(1, 100, ErrorMessage = "The Display Order Value should be between 1 and 100")]
+        public int DisplayOrder { get; set; }
+
+    }
+}
---
+++ AbbyWeb/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AbbyWeb.Data;
+using AbbyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AbbyWeb.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public IEnumerable<Category> Categories { get; set; }
+
+        public IndexModel(ApplicationDbContext db )
+        {
+            _db = db;
+        }
+
+        public void OnGet()
+        {
+            Categories = _db.Categories;
+        }
+    }
+}
